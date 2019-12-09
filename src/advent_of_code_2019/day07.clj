18d1532b9b836787f36b1935bec1a67cb2f6f2fa
Marcus Wangnusson <marxama@gmail.com>
2019-12-09 20:34:09 +0100

    Solve day 7 part 2

--- src/advent_of_code_2019/day07.clj
+++ src/advent_of_code_2019/day07.clj
@@ -2,17 +2,44 @@
   (:require [advent-of-code-2019.intcode :as intcode]
             [advent-of-code-2019.utils :as utils]))
 
-(defn run-amplifier-sequence [program phase-settings]
-  (->> phase-settings
-       (reduce (fn [{[output] :output} phase-setting]
-                 (intcode/run-program program [phase-setting output]))
-               {:output [0]})
-       :output
-       first))
+(defn build-amplifiers [program]
+  (->> program intcode/build-program (repeat 5)))
+
+(defn run-amplifier-sequence
+  [amplifiers initial-input phase-settings]
+  (->> (interleave amplifiers phase-settings)
+       (partition 2)
+       (reductions (fn [{output :output} [amplifier phase-setting]]
+                     (intcode/run-program amplifier (if phase-setting
+                                                      (cons phase-setting output)
+                                                      output)))
+                   {:output initial-input})
+       (drop 1)))
+
+(defn get-last-amplifier-output [amplifiers]
+  (-> amplifiers last :output first))
 
 (defn solve-part-1 []
-  (let [program (intcode/build-program (intcode/parse-input "resources/day07_input"))]
+  (let [amplifiers (build-amplifiers (intcode/parse-input "resources/day07_input"))]
     (->> (range 5)
          utils/permutations
-         (map #(run-amplifier-sequence program %))
+         (map #(run-amplifier-sequence amplifiers [0] %))
+         (map get-last-amplifier-output)
          (apply max))))
+
+(defn run-feedback-cycle [amplifiers initial-input phase-settings]
+  (let [new-amplifiers (run-amplifier-sequence amplifiers initial-input phase-settings)]
+    (if (-> new-amplifiers last :terminated?)
+      (get-last-amplifier-output new-amplifiers)
+      (recur (map #(assoc % :output []) new-amplifiers)
+             (-> new-amplifiers last :output)
+             (repeat nil)))))
+
+(defn solve-part-2
+  ([] (solve-part-2 (intcode/parse-input "resources/day07_input")))
+  ([program]
+   (let [amplifiers (build-amplifiers program)]
+     (->> (range 5 10)
+          utils/permutations
+          (map #(run-feedback-cycle amplifiers [0] %))
+          (apply max)))))
